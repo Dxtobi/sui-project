41c3f67ed30e825687dd0bd536bf06f9f6e10b5d
upraisertech <akoredesalaudeen54@gmail.com>
2024-05-04 15:54:36 +0100

    update-landing-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Typewriter from "@/components/shared/Typewriter";
 
 export default function Home() {
   useEffect(() => {
@@ -13,25 +14,45 @@ export default function Home() {
 
   return (
     <section className="flex w-full h-[140vh] bg-[#100146] text-white gap-12 px-12 lg:px-[4em] pt-[12em] justify-center items-start">
-      <div className="w-[38em]" data-aos="zoom-out-right" data-aos-anchor-placement="top-center">
+      <div
+        className="w-[38em]"
+        data-aos="zoom-out-right"
+        data-aos-anchor-placement="top-center"
+      >
         <h1 className="text-[48px] font-[600] leading-[60px]">
-          Writing Smart Contracts for the <span className="special-text">Sui Ecosystem</span> Just Got Better
+          Writing Smart Contracts for the{" "}
+          <span className="">
+            <Typewriter text="Suui Ecosystem" speed={200} />
+          </span>{" "}
+          Just Got Better
         </h1>
-        <p className='pt-4'>
-          Your All-in-One Solution for writing Move programming language code, generating smart contracts, facilitating cryptocurrency transactions
+        <p className="pt-4">
+          Your All-in-One Solution for writing Move programming language code,
+          generating smart contracts, facilitating cryptocurrency transactions
         </p>
-        <button className="bg-[radial-gradient(circle_at_bottom_right,_var(--tw-gradient-stops))] from-[#F258FF] to-[#9715FF] mt-8 p-4 rounded-md" type="button" data-aos="zoom-out-right" data-aos-anchor-placement="center-bottom">
+        <button
+          className="bg-[radial-gradient(circle_at_bottom_right,_var(--tw-gradient-stops))] from-[#F258FF] to-[#9715FF] mt-8 p-4 rounded-md"
+          type="button"
+          data-aos="zoom-out-right"
+          data-aos-anchor-placement="center-bottom"
+        >
           Explore Now with SuiAI &rarr;
         </button>
       </div>
 
-      <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" className="flex flex-col w-[37em] lg:w-[42em] justify-end items-end">
+      <div
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+        className="flex flex-col w-[37em] lg:w-[42em] justify-end items-end"
+      >
         <h1 className="special-text-2 text-[32px] font-[600]">
           Create a smart contract for a marketplace.
         </h1>
-        <img className='w-[62vh] h-42 mt-[-6rem] lg:mt-[-8rem] rounded-b-[70px]' src='/images/hero_AI.png' />
+        <img
+          className="w-[62vh] h-42 mt-[-6rem] lg:mt-[-8rem] rounded-b-[70px]"
+          src="/images/hero_AI.png"
+        />
       </div>
     </section>
   );
 }
-
